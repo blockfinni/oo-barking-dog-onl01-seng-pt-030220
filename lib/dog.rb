3f9c29375300a7a9c6787157625d959b3c6bada5
Benjamin A Ajisefinni Jr <aj.no.limit.1@gmail.com>
2020-03-29 23:15:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
 # Your code goes here!
+
+class Dog 
+  
+  
+  def name=(dogs_name)
+    
+    #instance variable to hold dogs name to be used 
+    @name_of_dog = dogs_name
+  end
+  
+  
+  def name
+    #instance variable to get name 
+    @name_of_dog
+  end
+  
+  
+  def bark=(dog_bark)
+    
+    #instance variable 
+    @dog_barks = dog_bark
+  end
+  
+  def bark
+    #instance variable to get bark
+    @dog_barks
+  end 
+  
+  
+end#end class
